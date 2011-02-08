37bd126c406370eaa1f6eec23b2e2deae2001d75
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-02-08 17:14:37 +0530

    Added logic to score based on earlier strikes

--- src/player_game.erl
+++ src/player_game.erl
@@ -34,25 +34,49 @@ score(Pid,Points) ->
 %%----------------------------------------------------------------- 
 
 init(PlayerName) ->
-    loop(PlayerName,0).
+    loop(PlayerName,0,{false, false}).
 
 %%----------------------------------------------------------------- 
 %% Loop to continuously receive messages about player performance
 %%----------------------------------------------------------------- 
 
-loop(PlayerName, Score) ->
+loop(PlayerName, Score, {LastStrike, _PriorToLastStrike}=Strike) ->
+    % The next line is only temporary. Will be removed later
+    io:format("Looping. Player:~p current score:~p, pending: ~p~n",
+	      [PlayerName, Score, Strike]),
     receive
+	% Handle stop message if received
 	{From, stop} ->
             From ! {message, {PlayerName, {status, stopped}, {score, Score}}},
 	    ok;
+
+	% Handle score points message
 	{From, {points, Points}} ->
 	    if 
-		Points =< 10 ->
-		    From ! {message, {PlayerName, {scored, Points}, {score, Score + Points}}},
-		    loop(PlayerName,Score + Points);
+		% valid points are only between 0 and 10
+		Points >= 0 andalso Points =< 10 ->
+		    % Addition to the scores depend upon pending strikes also
+		    Addition = 
+			case Strike of
+			    {true, true} -> Points * 3;
+                            {true, false} -> Points * 2;
+			    {false, true} -> Points * 2;
+			    {false, false} -> Points
+			end,
+                    NextScore = Score + Addition,
+		    % Decrement pending for the next loop if it is greater than zero, since
+		    % we have accounted for it in Addition above
+		    From ! 
+			{message, {PlayerName, 
+				   {scored, Points}, 
+				   {added, Addition},  
+				   {score, NextScore},
+				   {pending, {Points =:=10, LastStrike}}}},
+		    loop(PlayerName,NextScore, {Points =:= 10, LastStrike});
 		true ->
-		    From ! {message, {PlayerName, {ignored_score, Points}, {score, Score}}},
-		    loop(PlayerName, Score)
+		    % this is an invalid input. So just ignore it
+		    From ! {message, {PlayerName, {ignored_score, Points}, {score, Score}, {pending, Strike}}},
+		    loop(PlayerName, Score, Strike)
 	    end
     end.
 
