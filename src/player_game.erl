f78e8c7d49c6149306c0412ba18a0d499af2f15b
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-03-09 18:08:27 +0530

    Major rewrite in progress. Modified player_game to be just a simple module, no longer a process / gen_server

--- src/player_game.erl
+++ src/player_game.erl
@@ -1,47 +1,29 @@
 -module(player_game).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([start_link/1,stop/1,score/2,get_state/1]).
--behaviour(gen_server).
+-export([create/1, play/2, get_score/1, at_turn_begin/1]).
 -author('Dhananjay Nene').
 
--include("../include/game_state.hrl").
-
-%%----------------------------------------------------------------- 
-%% Function to start the game for a particular player
-%%----------------------------------------------------------------- 
-
-start_link(PlayerName) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [PlayerName], []).
-
-%%----------------------------------------------------------------- 
-%% API to stop the game for a particular player
-%%----------------------------------------------------------------- 
-
-stop(Pid) ->
-    Pid ! {self(), stop},
-    ok.
-
-%%----------------------------------------------------------------- 
-%% API to get current status
-%%----------------------------------------------------------------- 
-
-get_state(Pid) ->
-    gen_server:call(Pid, get_state).
-
-%%----------------------------------------------------------------- 
-%% API to add score to player game
-%%----------------------------------------------------------------- 
-
-score(Pid,Pins) ->
-    gen_server:call(Pid,{pins,Pins}).
+%%-------------------------------------------------------------
+%% Record that contains the current game state for a player
+%%-------------------------------------------------------------
+-record(game_state, 
+	{
+	  player_name,
+	  frame,
+	  shot,
+	  bonus_shot,
+	  last_shot,
+	  prior_to_last_shot,
+	  max_pins,
+	  score
+	}).
 
 
 %%----------------------------------------------------------------- 
 %% Initialisation of process to keep track of player performance
 %%----------------------------------------------------------------- 
 
-init(PlayerName) ->
-    {ok, #game_state{
+create(PlayerName) ->
+    #game_state{
 	   player_name        = PlayerName,
 	   frame              = 1,
 	   shot               = 1,
@@ -49,21 +31,7 @@ init(PlayerName) ->
 	   last_shot          = normal,
 	   prior_to_last_shot = normal,
 	   max_pins           = 10,
-           score              =0}}.
-
-%%----------------------------------------------------------------- 
-%% Terminate behaviour
-%%----------------------------------------------------------------- 
-
-terminate(_Reason, _State) ->
-    ok.
-
-%%----------------------------------------------------------------- 
-%% Code change behaviour
-%%----------------------------------------------------------------- 
-
-code_change(_OldVersion, State, _Extra) ->
-     {ok, State}.
+           score              =0}.
 
 %%----------------------------------------------------------------- 
 %% Compute the exact addition to the player's score
@@ -103,16 +71,16 @@ compute_status(State, Pins, NextScore) ->
 		    if
 			% Toppled all the pins
 			Pins =:= State#game_state.max_pins ->
-			    State#game_state{
+			    {turn_continue, State#game_state{
 			      shot = 2, bonus_shot = true, last_shot = strike, 
 			      prior_to_last_shot = State#game_state.last_shot, 
-			      max_pins = 10, score = NextScore};
+			      max_pins = 10, score = NextScore}};
 			% Toppled some or zero pins
 			true ->
-			    State#game_state{
+			    {turn_continue, State#game_state{
 			      shot = 2, last_shot = normal, 
 			      prior_to_last_shot = State#game_state.last_shot, 
-			      max_pins = 10 - Pins, score = NextScore}
+			      max_pins = 10 - Pins, score = NextScore}}
 		    end;
 		% Second shot
 		2 ->
@@ -128,21 +96,21 @@ compute_status(State, Pins, NextScore) ->
 			    end,
 			    % Have to award one more shot in this frame since
 			    % player had a spare
-			    State#game_state{
+			    {turn_continue, State#game_state{
 			      shot = 3, bonus_shot = true, 
 			      last_shot = ThisShotStatus, 
 			      prior_to_last_shot = State#game_state.last_shot, 
-			      max_pins = 10, score = NextScore};
+			      max_pins = 10, score = NextScore}};
 			% Toppled some or zero pins but first shot was a strike
 			State#game_state.last_shot =:= strike ->
 			    % Have to award one more shot in this frame since
 			    % player had a strike earlier
-			    State#game_state{
+			    {turn_continue, State#game_state{
 			      shot = 3, bonus_shot = true, 
 			      last_shot = normal, 
 			      prior_to_last_shot = State#game_state.last_shot, 
 			      max_pins = State#game_state.max_pins - Pins, 
-			      score = NextScore};
+			      score = NextScore}};
 			% Toppled some or zero pins and first shot was not a strike
 			true ->
 			    {game_over, 
@@ -178,19 +146,19 @@ compute_status(State, Pins, NextScore) ->
 			Pins =:= State#game_state.max_pins ->
 			    % Strike - toppled all the pins
 			    % No more shots in this frame - advance to next
-			    State#game_state{
+			    {turn_over, State#game_state{
 			      frame = State#game_state.frame + 1, shot = 1, 
 			      last_shot = strike, 
 			      prior_to_last_shot = State#game_state.last_shot, 
 			      max_pins = 10, 
-			      score = NextScore};
+			      score = NextScore}};
 			true ->
-			    State#game_state{
+			    {turn_continue, State#game_state{
 			      shot = 2, 
 			      last_shot = normal, 
 			      prior_to_last_shot = State#game_state.last_shot, 
 			      max_pins = 10 - Pins, 
-			      score = NextScore}
+			      score = NextScore}}
 		    end;
 		2 ->
 		    ThisShotStatus = 
@@ -198,26 +166,20 @@ compute_status(State, Pins, NextScore) ->
 			    true -> spare;
 			    false -> normal
 			end,
-		    State#game_state{
+		    {turn_over, State#game_state{
 		      frame = State#game_state.frame + 1, shot = 1, 
 		      last_shot = ThisShotStatus, 
 		      prior_to_last_shot = State#game_state.last_shot, 
 		      max_pins = 10, 
-		      score = NextScore}
+		      score = NextScore}}
 	    end
     end.
-
-%%----------------------------------------------------------------- 
-%% Handle request to get state
-%%----------------------------------------------------------------- 
-
-handle_call({get_state},_From, State) -> {reply, State, State};
 
 %%----------------------------------------------------------------- 
 %% Handle the call to score based on pins being toppled
 %%----------------------------------------------------------------- 
 
-handle_call({pins, Pins}, _From, State) when Pins >= 0 andalso Pins =< State#game_state.max_pins ->
+play(Pins, State) when Pins >= 0 andalso Pins =< State#game_state.max_pins ->
     Addition = compute_addition(Pins, 
 				State#game_state.last_shot,
 				State#game_state.prior_to_last_shot,
@@ -225,35 +187,26 @@ handle_call({pins, Pins}, _From, State) when Pins >= 0 andalso Pins =< State#gam
     % Compute the addition to the score
            NextScore = State#game_state.score + Addition,
     % Compute ShotStatus, NextFrame, NextShot, NextMaxPins
-	    Val = compute_status(State, Pins, NextScore),
-    case Val of
-    	#game_state{} = NextState -> 
-    	    % Send Message to controller
-    	    Reply = {game_progressed, NextState, NextState#game_state.score};
-    	{game_over, NextState}  ->
-    	    Reply = {game_over, NextState},
-	    io:format("Game over with : ~p~n",[NextState])
-    end,
-    {reply, Reply, NextState};
-handle_call({pins,_Pins},_From,_State) ->
-    {error, invalid}.
+	    compute_status(State, Pins, NextScore);
+play(Pins,State) ->
+    {invalid_input, 
+     Pins, 
+     io_lib:format("Pins must be less than or equal to ~p",
+		  [State#game_state.max_pins]),
+     State}.
 
 
 %%----------------------------------------------------------------- 
-%% Handle casts. Currently none anticipated.
+%% Get the current score
 %%----------------------------------------------------------------- 
 
-handle_cast(fail, State) ->
-    1/0,
-    {noreply, State};
-handle_cast(Request, State) ->
-    io:format("Received a cast ~w~n",[Request]),
-    {noreply, State}.
+get_score(State) ->
+    State#game_state.score.
 
 %%----------------------------------------------------------------- 
-%% Handle info messages. Currently none planned.
+%% Is the player at the beginning of a frame?
 %%----------------------------------------------------------------- 
 
-handle_info(Info, State) ->
-    io:format("Received a info ~w~n",[Info]),
-    {noreply, State}.
+at_turn_begin(State) ->
+    State#game_state.shot =:= 1.
+
